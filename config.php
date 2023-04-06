dda77d8a12d31aeb20ddeda115305f9735a04345
Chad Cooper <chadcooper06@gmail.com>
2023-04-06 16:20:17 -0400

    Update config.php

--- config.php
+++ config.php
@@ -2,9 +2,9 @@
 return array(
     'authorizationRequestUrl' => 'https://appcenter.intuit.com/connect/oauth2',
     'tokenEndPointUrl' => 'https://oauth.platform.intuit.com/oauth2/v1/tokens/bearer',
-    'client_id' => 'ABgMsihEejNnEqq2SBhvU14vLG62UT19Gy2vwu676wmmxLUEjd',
-    'client_secret' => '0d2Tz408mdWfZ4lXDJCvI3a4y1HwnwM9s7mTUSPy',
+    'client_id' => 'ABP4qgYkXDAUdx0zbKX509jgbtu2b0K49t13FZw4bbaWxa88MI',
+    'client_secret' => 'g5VURt6aRPxOVrT6dc22AqDc460lOfY2QiQAiyXZ',
     'oauth_scope' => 'com.intuit.quickbooks.accounting openid profile email phone address',
-    'oauth_redirect_uri' => 'http://localhost:3000/callback.php',
+    'oauth_redirect_uri' => 'http://localhost:3000/callback',
 )
 ?>
