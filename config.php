b6262f7e503b869ae22ecb9b637486c03c2e633b
Chad Cooper <chadcooper06@gmail.com>
2023-04-06 11:48:41 -0400

    Update config.php

--- config.php
+++ config.php
@@ -2,8 +2,8 @@
 return array(
     'authorizationRequestUrl' => 'https://appcenter.intuit.com/connect/oauth2',
     'tokenEndPointUrl' => 'https://oauth.platform.intuit.com/oauth2/v1/tokens/bearer',
-    'client_id' => 'Enter the clietID from Developer Portal',
-    'client_secret' => 'Enter the clientSecret from Developer Portal',
+    'client_id' => 'ABgMsihEejNnEqq2SBhvU14vLG62UT19Gy2vwu676wmmxLUEjd',
+    'client_secret' => '0d2Tz408mdWfZ4lXDJCvI3a4y1HwnwM9s7mTUSPy',
     'oauth_scope' => 'com.intuit.quickbooks.accounting openid profile email phone address',
     'oauth_redirect_uri' => 'http://localhost:3000/callback.php',
 )
